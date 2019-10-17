461138d3921037a7bde125a148b5464f543fe3a5
jtedmonson <35669793+jtedmonson@users.noreply.github.com>
2019-10-16 23:23:19 -0500

    Add files via upload

---
+++ OracleDataAdapter.java
@@ -0,0 +1,21 @@
+
+public class OracleDataAdapter implements IDataAdapter {
+    public int connect(String dbfile) {
+        //...
+        return CONNECTION_OPEN_OK;
+    }
+
+    public int disconnect() {
+        // ...
+        return CONNECTION_CLOSE_OK;
+
+    }
+
+    public ProductModel loadProduct(int id) {
+        return null;
+    }
+    public int saveProduct(ProductModel model) {
+        return PRODUCT_SAVED_OK;
+    }
+
+}
